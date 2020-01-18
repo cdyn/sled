6172e299c881142eafad3ac980f3e430ab9d89b8
JERiv <jrunnells@ccomplabs.com>
2020-01-18 06:31:06 -0500

    Update sled.ino

--- sled/sled.ino
+++ sled/sled.ino
@@ -8,7 +8,7 @@
   jrunnells@customdyn.com
 */
 
-//LED PINS
+//LED PNP PINS
 int gPin = 9;
 int rPin = 10;
 int bPin = 13;
@@ -18,9 +18,8 @@ int red = 255;
 int green = 100;
 int blue = 50;
 
+//
 int mode = 0; //MODE -1 == off, 0 == rgb, 1 == fade
-unsigned long tick = 30; //Step Duration
-unsigned long lastT = 0;
 
 char serial_command_buffer_[32];
 SerialCommands serial_commands_(&Serial, serial_command_buffer_, sizeof(serial_command_buffer_), "\r\n", " ");
@@ -52,71 +51,138 @@ void cmd_unrecognized(SerialCommands* sender, const char* cmd) {
 }
 
 void cmd_rgba (SerialCommands* sender) {
-  //Note: Every call to Next moves the pointer to next parameter
-
-  char* pwm_str = sender->Next();
-  if (pwm_str == NULL)
-  {
-    sender->GetSerial()->println("ERROR NO_PWM");
+  //R
+  char* r_str = sender->Next();
+  if (r_str == NULL){
+    sender->GetSerial()->println("ERROR: EXPECTED R");
+    return;
+  } int r = atoi(r_str);
+  //G
+  char* g_str = sender->Next();
+  if (g_str == NULL){
+    sender->GetSerial()->println("ERROR: EXPECTED G");
+    return;
+  } int g = atoi(g_str);
+  //B
+  char* b_str = sender->Next();
+  if (b_str == NULL){
+    sender->GetSerial()->println("ERROR: EXPECTED B");
+    return;
+  } int b = atoi(b_str);
+  //A
+  char* a_str = sender->Next();
+  if (a_str == NULL){
+    sender->GetSerial()->println("ERROR: EXPECTED A");
+    return;
+  } int a = atoi(a_str);
+  if(sender->Next() != null){
+    sender->GetSerial()->println("ERROR: TOO MANY ARGS");
     return;
   }
-  int pwm = atoi(pwm_str);
-
-  char* led_str;
-  while ((led_str = sender->Next()) != NULL)
-  {
-    if (set_led(led_str, pwm))
-    {
-      sender->GetSerial()->print("LED_STATUS ");
-      sender->GetSerial()->print(led_str);
-      sender->GetSerial()->print(" ");
-      sender->GetSerial()->println(pwm);
-    }
-    else
-    {
-      sender->GetSerial()->print("ERROR ");
-      sender->GetSerial()->println(led_str);
-    }
-  }
+  //bound input 0 <= x <= 255
+  if(r < 0){r=0;} else if(r > 255){r=255;}
+  if(g < 0){g=0;} else if(g > 255){g=255;}
+  if(b < 0){b=0;} else if(b > 255){b=255;}
+  if(a < 0){a=0;} else if(a > 255){a=255;}
+  //notify
+  sender->GetSerial()->print("Setting (R,G,B,A): (");
+  sender->GetSerial()->print(r);
+  sender->GetSerial()->print(",");
+  sender->GetSerial()->print(g);
+  sender->GetSerial()->print(",");
+  sender->GetSerial()->print(b);
+  sender->GetSerial()->print(",");
+  sender->GetSerial()->print(a);
+  sender->GetSerial()->println(")");
+  //alpha adjust
+  red = (r*a)/255;
+  green = (g*a)/255;
+  blue = (b*a)/255;
+  //set mode and exec
+  mode = 0;
+  set_rgb(red, green, blue);
 }
 
 void cmd_off (SerialCommands* sender) {
-  //Note: Every call to Next moves the pointer to next parameter
+  if(sender->Next() != null){
+    sender->GetSerial()->println("ERROR: TOO MANY ARGS");
+    return;
+  }
+  //toggle on/off
   if(mode == -1){
     mode = 0;
+    sender->GetSerial()->println("Turning ON");
     set_rgb(red,green,blue);
   } else {
+    sender->GetSerial()->println("Turning OFF");
     mode = -1;
   }
 }
 
 void cmd_fade(SerialCommands* sender) {
-  //Note: Every call to Next moves the pointer to next parameter
-
-  char* pwm_str = sender->Next();
-  if (pwm_str == NULL)
-  {
-    sender->GetSerial()->println("ERROR NO_PWM");
+  if(mode == 1){//prohibit stack trace overflow from recursive function call since fade() can also ReadSerial()
+    mode == -1;
+    off();
+    sender->GetSerial()->println("Fade running, turning OFF");
     return;
   }
-  int pwm = atoi(pwm_str);
-
-  char* led_str;
-  while ((led_str = sender->Next()) != NULL)
-  {
-    if (set_led(led_str, pwm))
-    {
-      sender->GetSerial()->print("LED_STATUS ");
-      sender->GetSerial()->print(led_str);
-      sender->GetSerial()->print(" ");
-      sender->GetSerial()->println(pwm);
-    }
-    else
-    {
-      sender->GetSerial()->print("ERROR ");
-      sender->GetSerial()->println(led_str);
-    }
+  //R
+  char* r_str = sender->Next();
+  if (r_str == NULL){
+    sender->GetSerial()->println("ERROR: EXPECTED R");
+    return;
+  } int r = atoi(r_str);
+  //G
+  char* g_str = sender->Next();
+  if (g_str == NULL){
+    sender->GetSerial()->println("ERROR: EXPECTED G");
+    return;
+  } int g = atoi(g_str);
+  //B
+  char* b_str = sender->Next();
+  if (b_str == NULL){
+    sender->GetSerial()->println("ERROR: EXPECTED B");
+    return;
+  } int b = atoi(b_str);
+  //A
+  char* a_str = sender->Next();
+  if (a_str == NULL){
+    sender->GetSerial()->println("ERROR: EXPECTED A");
+    return;
+  } int a = atoi(a_str);
+  //T
+  char* t_str = sender->Next();
+  if (t_str == NULL){
+    sender->GetSerial()->println("ERROR: EXPECTED T");
+    return;
+  } unsigned long t = strtoul(t_str, NULL, 10);
+  if(sender->Next() != null){
+    sender->GetSerial()->println("ERROR: TOO MANY ARGS");
+    return;
   }
+  //bound input 0 <= x <= 255
+  if(r < 0){r=0;} else if(r > 255){r=255;}
+  if(g < 0){g=0;} else if(g > 255){g=255;}
+  if(b < 0){b=0;} else if(b > 255){b=255;}
+  if(a < 0){a=0;} else if(a > 255){a=255;}
+  //notify
+  sender->GetSerial()->print("Fading to (R,G,B,A): (");
+  sender->GetSerial()->print(r);
+  sender->GetSerial()->print(",");
+  sender->GetSerial()->print(g);
+  sender->GetSerial()->print(",");
+  sender->GetSerial()->print(b);
+  sender->GetSerial()->print(",");
+  sender->GetSerial()->print(a);
+  sender->GetSerial()->print(") in(ms):");
+  sender->GetSerial()->println(t);
+  //alpha adjust
+  r = (r*a)/255;
+  g = (g*a)/255;
+  b = (b*a)/255;
+  //set mode and exec
+  mode = 1;
+  fade(r,g,b,t);
 }
 
 void set_rgb(int r, int g, int b){
@@ -125,53 +191,36 @@ void set_rgb(int r, int g, int b){
   analogWrite(bPin, b);
 }
 
-void rgba(int r, int g, int b, int a) {//set rgb with 'a' represeneting 'alpha' or brightness as a fraction of 255
-  //bound input 0 <= x <= 255
-  if(r < 0){r=0;} else if(r > 255){r=255;}
-  if(g < 0){g=0;} else if(g > 255){g=255;}
-  if(b < 0){b=0;} else if(b > 255){b=255;}
-  if(a < 0){a=0;} else if(a > 255){a=255;}
-  //preform alpha calculation
-  red = (r*a)/255;
-  green = (g*a)/255;
-  blue = (b*a)/255;
-  set(red, green ,blue);
-}
-
 void off(){
   set_rgb(0,0,0);
 }
 
-void fade(int r, int g, int b, int a, int t){
+void fade(int r, int g, int b, unsigned long t){//same as set_rbg(r,b,g) with a time component 't' in milliseconds
+  unsigned long lastT = millis(); //Last tick
+  unsigned long stepD = millis() - lasT; //Step Duration
   while (mode == 1) {
-    
-    for (int fadeValue = 0; fadeValue <= 255; fadeValue +=5){
-      analog
-      delay(t);
-    }
-    
-    for (int fadeValue = 255; fadeValue >= 0; fadeValue -= 5){
+    if(millis() < lastT){lasT = millis();}//handle 50 hr clock overflow
+    stepD = millis() - lastT; // update step duration
+    if(stepD > t){ //new tick
+      if((lastT / t) % 2 == 1){//stepping up
+        set_rgb();
+      } else if((lastT / t) % 2 == 0) {//stepping down
+        set_rgb();
+      }
       
     }
-    
+    serial_commands_.ReadSerial();
   }
 }
 
 void loop() {
-  if(mode == -1){
-    off();
-  } else if(mode == 0){
-    set_rgb(red,green,blue);
-  } else if(mode == 1){//note fade diverts the main loop
-    //fade(); 
-  }
-  serial_commands_.ReadSerial();
-  
-  /*// fade in from min to max in increments of 5 points:
+  serial_commands_.ReadSerial();  
+}
+
+/*// fade in from min to max in increments of 5 points:
   for (int fadeValue = 0 ; fadeValue <= 255; fadeValue += 5) {
     // sets the value (range from 0 to 255):
     analogWrite(gPin, fadeValue);
     // wait for 30 milliseconds to see the dimming effect
     delay(30);
   }*/
-}
